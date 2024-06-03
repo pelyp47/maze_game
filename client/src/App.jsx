09a986a5107453423e638780b3498dc91c0bcd99
DmytroPelypHaloLab <dmytro.p@halo-lab.team>
2024-06-03 15:02:36 +0200

    fix: start script

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,50 +1,59 @@
-"use client"
-import './App.css'
-import {useRouter} from 'next/navigation';
-import { signIn, useSession } from 'next-auth/react';
-import { useEffect, useState } from 'react';
+"use client";
+import "./App.css";
+import { useRouter } from "next/navigation";
+import { signIn, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 function App() {
-  const router = useRouter()
-  const session = useSession()
-  
-  const [loggedIn, setLoggedIn] = useState(false)
+  const router = useRouter();
+  const session = useSession();
+
+  const [loggedIn, setLoggedIn] = useState(false);
   const [name, setName] = useState(localStorage.getItem("name"));
   const [id, setId] = useState(localStorage.getItem("id"));
   
-  localStorage.setItem("name", localStorage.getItem("name")||"")
-  localStorage.setItem("id", localStorage.getItem("id")||"")
-  useEffect(()=>{
-    localStorage.setItem("name", localStorage.getItem("name")||"")
-    localStorage.setItem("id", localStorage.getItem("id")||"")
-    const checkLoggedIn = async ()=>{
+  localStorage.setItem("name", localStorage.getItem("name") || "");
+  localStorage.setItem("id", localStorage.getItem("id") || "");
+  useEffect(() => {
+    localStorage.setItem("name", localStorage.getItem("name") || "");
+    localStorage.setItem("id", localStorage.getItem("id") || "");
+    const checkLoggedIn = async () => {
       
       const fetchData = await fetch(`/api/checkUser/`, {
         method: "POST",
-        body: JSON.stringify({name})
-      })
-      const data = await fetchData.json()
-      setName(data.name||name);
-      setId(data.id||id);
-      setLoggedIn(data.loggedIn);
-      
-      localStorage.setItem("name", data.name||name);
-      localStorage.setItem("id", data.id||id)
-      if(data.loggedIn) {
-        router.push(`/Home?id=${data.id}&name=${data.name}&loggedIn=${data.loggedIn}`)
-      } else {
-        router.push(`/LogInForm`)
+        body: JSON.stringify({ name }),
+      });
+      let data = {};
+      try {
+        data = await fetchData.json();
+      } catch (e) {
+        console.log('Unable to fetch user data');
       }
+      setName(data?.name || name);
+      setId(data?.id || id);
+      setLoggedIn(data?.loggedIn);
+
+      localStorage.setItem("name", data.name || name);
+      localStorage.setItem("id", data.id || id);
+      if (data.loggedIn) {
+        router.push(
+          `/Home?id=${data.id}&name=${data.name}&loggedIn=${data.loggedIn}`
+        );
+      } else {
+        router.push(`/LogInForm`);
+      }
+    };
+    checkLoggedIn();
+  }, [router, loggedIn, id, name]);
+  useEffect(() => {
+    if (session.status !== "loading" && !session.data) {
+      signIn("first", null, { login_hint: "dmytro.p@halo-lab.team" });
     }
-    checkLoggedIn()
-  }, [router, loggedIn, id, name])
-  useEffect(()=>{
-    if(session.status!=="loading"&&!session.data) {
-      signIn('first',  null, { login_hint: "dmytro.p@halo-lab.team" })
-    }
-  }, [session])
-  return (<>
-    <span>...</span>
-  </>)
+  }, [session]);
+  return (
+    <>
+      <span>...</span>
+    </>
+  );
 }
 
-export default App
+export default App;
--- client/src/i18n.js
+++ client/src/i18n.js
@@ -2,7 +2,7 @@
 import {getRequestConfig} from 'next-intl/server';
  
 export default getRequestConfig(async () => {
-  const locale = 'ua';
+  const locale = 'en';
  
   return {
     locale,
